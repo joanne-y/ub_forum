85dbb58cac851f219055eb6716b905e7039714d9
Joanne Yong <2019120152@ub.edu.bz>
2022-12-02 16:39:28 -0800

    Allowed for the trusted origins to be provided via command line through a flag

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,6 +6,7 @@ import (
 	"database/sql"
 	"flag"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -40,6 +41,9 @@ type config struct {
 		password string
 		sender   string
 	}
+	cors struct {
+		trustedOrigins []string
+	}
 }
 
 // Dependency Injection
@@ -67,6 +71,12 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "828352fbf5560b", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "9616ffbf8b5bad", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "UBforum <no-reply@ubforum.joanneyong.net>", "SMTP sender")
+	// Use the flag.Func() function to parse our trusted origins flag from
+	// a string to a slice of a string
+	flag.Func("cors-trusted-origin", "Trusted CORS origin (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
 	flag.Parse()
 	// Create a logger
 	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
